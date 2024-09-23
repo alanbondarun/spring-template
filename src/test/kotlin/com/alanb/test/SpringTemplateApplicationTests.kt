477aa70fc0bb9bbe3bbe4bd5f1212363bf0eff0d
alanb <efik722life@gmail.com>
2024-09-23 18:54:51 +0900

    feat: add base project via spring initializr

---
+++ src/test/kotlin/com/alanb/test/SpringTemplateApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alanb.test
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringTemplateApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
