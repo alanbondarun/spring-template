ed70fcf019b91b85e068ff5b48e1974796fa3b68
alanb <efik722life@gmail.com>
2024-11-03 09:35:40 +0900

    style: add ktlint and detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
 	alias(libs.plugins.kotlin.spring)
 	alias(libs.plugins.kotlin.jpa)
 	alias(libs.plugins.spring.boot)
+	alias(libs.plugins.ktlint)
+	alias(libs.plugins.detekt)
 }
 
 group = "com.alanb.test"
